64743c8df6f9f2d07bcf4509cfd2fa38f70bf01e
fengjinlong <18618166564@163.com>
2019-11-25 17:08:57 +0800

    流式

--- src/server/controllers/IndexController.js
+++ src/server/controllers/IndexController.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 import Controller from "./BaseController";
+const { Readable } = require('stream')
 class IndexController extends Controller {
     constructor() {
         super();
@@ -13,7 +14,23 @@ class IndexController extends Controller {
         ctx.body = await ctx.render('index/index')
     }
     async homeIndex(ctx, next) {
-        ctx.body = await ctx.render('home/home')
+        console.log(await ctx.render('home/home'))
+        console.log(111)
+        // ctx.body = await ctx.render('home/home')
+        let result = await ctx.render('home/home')
+
+        // 流式传送
+        ctx.status = 200
+        ctx.type = 'html'
+        function createSsrStreamPromise() {
+            return new Promise((resolve, reject) => {
+            let stream = new Readable()
+            stream.push(result)
+            stream.push(null)
+            stream.on('error', err => { reject(err) }).pipe(ctx.res)
+            })
+        }
+        await createSsrStreamPromise()
     }
 }
 export default IndexController;
